b1fc62097c60471beb400d802b360537ede2ff2e
llightcb <doleovit@tuta.io>
2023-02-07 15:34:57 +0100

    pv: cleanup

--- home/.config/fish/functions/pv.fish
+++ home/.config/fish/functions/pv.fish
@@ -2,12 +2,6 @@ function pv
     argparse -X 0 -- $argv
     or return
 
-    apk -eq info pipe-viewer
-    or begin
-        echo "@edge/testing"
-        return 1
-    end
-
-    pipe-viewer --order=upload_date --ignore-av1 --prefer-m4a --prefer-mp4 \
+    pipe-viewer --order=relevance --ignore-av1 --prefer-m4a --prefer-mp4 \
     --player=mpv --append-arg="--profile=streamit --no-ytdl" --resolution=720p
 end

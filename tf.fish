a9acd42f8043790a7230ba9afc2969db90e61c68
llightcb <doleovit@tuta.io>
2023-02-20 21:31:53 +0100

    fish: - tf

--- tf.fish
+++
@@ -1,9 +0,0 @@
-function tf
-    if set -q argv[1]
-        set -l tmpfile (mktemp -t transferXXXXXX)
-        curl --progress-bar --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) -H "Max-Downloads: 1" >>$tmpfile
-        cat $tmpfile; command rm -f $tmpfile
-    else
-        echo 'usage: $ tf FILE_TO_TRANSFER'
-    end
-end

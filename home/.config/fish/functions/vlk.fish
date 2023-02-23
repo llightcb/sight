69c64bd3ee3b7a95f8d6a486ec74a991a2fa95cf
llightcb <doleovit@tuta.io>
2023-02-23 23:12:42 +0100

    + vlk

---
+++ home/.config/fish/functions/vlk.fish
@@ -0,0 +1,16 @@
+function vlk --description 'vlock-screen'
+    argparse -X 0 -- $argv
+    or return
+
+    set -l cr (cat /sys/devices/virt*/tty/tty0/active)
+
+    if test "$cr" = tty1
+        doas openvt -s -- doas -u (whoami) $SHELL -c \
+        'vlock -a; chvt 1; doas deallocvt'
+        echo "
+        welcome back!
+        "
+    else
+        return 1
+    end
+end

c499aa11738e20f6faf4a0839c5f4a38d9737e06
llightcb <doleovit@tuta.io>
2023-04-16 12:14:45 +0200

    gd: ~

--- home/.config/fish/functions/gd.fish
+++ home/.config/fish/functions/gd.fish
@@ -1,5 +1,7 @@
 function gd
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1
-        git diff --color=always $argv | less -R -i -w
+        git diff --color $argv | diff-highlight | less -Riw
+    else
+        return 1
     end
 end

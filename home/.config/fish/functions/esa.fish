cf38b79470def0a0fe878e5e2a72e714e9d34d04
llightcb <doleovit@tuta.io>
2023-06-28 17:39:08 +0200

    esa: misc

--- home/.config/fish/functions/esa.fish
+++ home/.config/fish/functions/esa.fish
@@ -1,12 +1,34 @@
-function esa -d 'basic'
+function esa --description 'basic ssh'
     argparse -X 0 -- $argv
     or return
 
-    if test -z (pgrep -f ssh-agent)
-        eval (ssh-agent -c) >/dev/null
-        ssh-add
-        trap "
-        kill $SSH_AGENT_PID
-        " EXIT
+    if test -z "$SSH_AGENT_PID"
+        if ! pgrep ssh-agent >/dev/null
+            read -l -P \
+            'lifetime in minutes: ' cho
+            test -n "$cho"; or return 1
+            function nf --on-signal INT
+                echo "esa: cleaned up!"
+                wtype -k return # lazy?
+            end
+            set -l mn (math $cho \* 60)
+            eval $(ssh-agent -t $mn -c)
+            ssh-add; trap '
+            set -e SSH_AUTH_SOCK
+            kill $SSH_AGENT_PID
+            set -e SSH_AGENT_PID
+            trap - INT TERM EXIT
+            functions -e nf
+            ' INT TERM EXIT
+            return 0 # builtin trap = ↓
+        else
+            echo "
+            → another session is active
+            "; return 1
+        end
+    else
+        set --show \
+            SSH_AUTH_SOCK SSH_AGENT_PID
+        return 0
     end
 end

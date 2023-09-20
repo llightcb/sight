abe4bc9d36e169ece27130392ca27aac634b12de
llightcb <llightcb@gmail.com>
2023-09-20 20:41:45 +0200

    pw: misc

--- home/.config/fish/functions/pw.fish
+++ home/.config/fish/functions/pw.fish
@@ -4,7 +4,9 @@ function pw
     argparse -X1 $opts -- $argv
     or return
 
-    set -f iter 500000
+    set -f iter 1000000
+    set -f -x LESSSECURE 1
+    set -f algo aes-256-cbc
 
     if set -q _flag_v
         if test -e "$argv[1]"
@@ -12,8 +14,15 @@ function pw
                 set -l if_r $argv[1]
                 read -slP 'pwd: ' pr
                 set -l hasr (echo -n -- $pr | openssl dgst -sha256)
-                less -m -n -q -S -w -i (openssl enc -aes-256-cbc -salt -md sha256 -pbkdf2 \
-                -iter $iter -a -d -in $if_r -pass file:(printf %s $hasr | psub) | psub)
+                set -l decrypt_cont (openssl enc -{$algo} -salt -md sha256 -pbkdf2 \
+                -iter $iter -a -d -in $if_r -pass file:(printf %s $hasr | psub) 2>&1)
+                if not test "$status" -eq 0
+                    echo "error: wrong password. decryption failed"
+                    return 1
+                else
+                    less -RnSic (printf %s\\n $decrypt_cont | psub)
+                    return 0
+                end
             else
                 return 1
             end
@@ -34,14 +43,17 @@ function pw
                     return 1
                 end
                 set -l hase (echo -n -- $pe | openssl dgst -sha256)
-                openssl enc -aes-256-cbc -salt -md sha256 -pbkdf2 -iter $iter -a -in $if_e \
+                openssl enc -{$algo} -salt -md sha256 -pbkdf2 -iter $iter -a -in $if_e \
                 -out $of_e -pass file:(printf %s "$hase" | psub)
+                if not test "$status" -eq 0
+                    return 1
+                end
                 printf \\n; echo ".... encoded file: $PWD/$of_e"
                 while true
                     read -l -P "remove file: \"$if_e\" (y/n) " ch
                     switch $ch
                         case y
-                            shred -z -u -- $if_e
+                            /bin/rm -v -- $if_e
                             return 0
                         case n
                             return 0
@@ -63,10 +75,15 @@ function pw
                 set -l of_d (string sub -e -4 -- $if_d)
                 read -slP 'pwd: ' pd
                 set -l hasd (echo -n -- $pd | openssl dgst -sha256)
-                openssl enc -aes-256-cbc -salt -md sha256 -pbkdf2 -iter $iter -a -d -in $if_d \
+                openssl enc -{$algo} -salt -md sha256 -pbkdf2 -iter $iter -a -d -in $if_d \
                 -pass file:(printf %s "$hasd" | psub) -out $of_d
-                and /bin/rm -i -v -- $if_d
-                echo ".. decoded file: $PWD/$of_d"
+                if test "$status" -eq 0
+                    /bin/rm -iv -- $if_d
+                    echo ".. decoded file: $PWD/$of_d"
+                else
+                    /bin/rm -- $of_d
+                    return 1
+                end
             else
                 return 1
             end

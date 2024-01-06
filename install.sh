0ddae8ae3fe38eb291a22b4b8b9ea1b4bdfc72a9
llightcb <llightcb@gmail.com>
2024-01-06 18:37:18 +0100

    sh: fix/+pkg

--- install.sh
+++ install.sh
@@ -134,6 +134,7 @@ EOF
     xdg-utils swayidle
     man-db doas-doc
     brightnessctl
+    efibootmgr
     apk-tools-doc
     git-diff-highlight
     pipewire nftables shfmt
@@ -201,6 +202,11 @@ EOF
         rc-update -q -q del hwclock -a
         rc-update -q add osclock boot
     fi
+
+    # jinc
+    sed -Ei \
+        's|^#(RESOLV_CONF="no")|\1|' \
+        /etc/udhcpc/udhcpc.conf
 
     # rvcf
     cut -c 5- <<EOF > /etc/resolv.conf
@@ -428,7 +434,7 @@ EOF
 EOF
 
     # pdin
-    sed -Ei 's|^#(previous_dmesg=yes)|\1|' \
+    sed -Ei 's|^#(previous_dmesg).*|\1=yes|' \
     /etc/conf.d/bootmisc
 
     # imvc
@@ -474,13 +480,6 @@ EOF
 
     # cdlv
     echo "rc_verbose=yes" > /etc/conf.d/local
-
-    # jinc
-    mkdir -p /etc/udhcpc
-
-    cut -c 5- <<EOF > /etc/udhcpc/udhcpc.conf
-    RESOLV_CONF="no"
-EOF
 
     # xbel
     cut -c5- <<EOF \
@@ -635,6 +634,8 @@ EOF
         swp="$(grep -E -w '(partition|file)' $prc)"
         sed -i "s/LINUX_DEFAULT=\"/&$pa${swp:+$zs}/" /etc/default/grub
         grub-mkconfig --output=/boot/grub/grub.cfg
+        gr_r=fish/functions; mkdir -p /root/.config/"$gr_r" # grinroot
+        cp sight/home/.config/"$gr_r"/grin.fish /root/.config/"$gr_r"/
     fi
 
     # sbsh
@@ -784,5 +785,6 @@ EOF
     chmod -R go-srwx "$dir"; seq 1 3 \
     | tr -dc \\n; getent passwd "$usn"
 
-    printf \\n"
-    \033[37;7mdone!\n reboot:\033[0m "
+    printf "
+    \n\033[37;7mnext:\n step 4 @README
+    if VM:reboot right now\033[0m\n\n"

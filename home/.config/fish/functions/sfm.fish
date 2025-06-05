26dc3c04bf2476186b99c39854b60e65dc48be0b
llightcb <llightcb@gmail.com>
2025-06-05 11:57:06 +0200

    sfm: -flag

--- home/.config/fish/functions/sfm.fish
+++ home/.config/fish/functions/sfm.fish
@@ -22,31 +22,31 @@ function sfm
             → ' choice
             switch $choice
                 case a
-                    set -f url (curl -sLf -4 'https://somafm.com/deepspaceone32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
+                    set -f url (curl -sf -4 'https://somafm.com/deepspaceone32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
                     break
                 case d
-                    set -f url (curl -sLf -4 'https://somafm.com/dronezone32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
+                    set -f url (curl -sf -4 'https://somafm.com/dronezone32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
                     break
                 case n
-                    set -f url (curl -sLf4 'https://somafm.com/n5md32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
+                    set -f url (curl -sf -4 'https://somafm.com/n5md32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
                     break
                 case v
-                    set -f url (curl -sLf4 'https://somafm.com/live32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
+                    set -f url (curl -sf -4 'https://somafm.com/live32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
                     break
                 case l
-                    set -f url (curl -sLf4 'https://somafm.com/metal32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
+                    set -f url (curl -sf -4 'https://somafm.com/metal32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
                     break
                 case u
-                    set -f url (curl -sLf4 'https://somafm.com/u80s32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
+                    set -f url (curl -sf -4 'https://somafm.com/u80s32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f 2-)
                     break
                 case f
-                    set -f url (curl -sLf4 'https://somafm.com/defcon32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
+                    set -f url (curl -sf -4 'https://somafm.com/defcon32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
                     break
                 case c
-                    set -f url (curl -sLf4 'https://somafm.com/spacestation32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
+                    set -f url (curl -sf -4 'https://somafm.com/spacestation32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
                     break
                 case j
-                    set -f url (curl -sLf -4 'https://somafm.com/sonicuniverse32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
+                    set -f url (curl -sf -4 'https://somafm.com/sonicuniverse32.pls' 2>&1 | grep -i '^file1' | cut -d '=' -f2-)
                     break
                 case x
                     return 0

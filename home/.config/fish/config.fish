b70f083984962f924340635a6ec9dc735a9ccf54
llightcb <llightcb@gmail.com>
2025-07-16 10:34:51 +0200

    ~fa

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # commands to run in interactive sessions can go here
+    abbr -a fa 'unshare -rn -- fastfetch -c all -l small'
     abbr -a gl 'git log --graph --decorate --oneline'
     abbr -a gpf 'git push --force-with-lease'
     abbr -a hdc ' history delete --contains'
-    abbr -a fa ' fastfetch -c all -l small'
     abbr -a rs 'command printf "\033c"'
     abbr -a gri 'git rebase -i HEAD~'
     abbr -a gba 'git branch -a'

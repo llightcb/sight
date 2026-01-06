7d2ab3709ba89b05d604b94e503b71d95849de49
llightcb <llightcb@gmail.com>
2026-01-06 09:39:02 +0100

    fish v4.3.2: ~scope_U2g

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,35 @@
 if status is-interactive
-    # commands to run in interactive sessions can go here
+    ## theme
+    set -g fish_pager_color_selected_background --background=FF9940
+    set -g fish_pager_color_progress brwhite --background=cyan
+    set -g fish_pager_color_prefix normal --bold --underline
+    set -g fish_color_search_match --background=FF9940
+    set -g fish_color_selection --background=FF9940
+    set -g fish_pager_color_description B3A06D
+    set --global fish_color_status red
+    set --global fish_color_end ED9366
+    set --global fish_color_cwd 399EE6
+    set --global fish_color_host normal
+    set --global fish_color_user brgreen
+    set --global fish_color_match F07171
+    set --global fish_color_cwd_root red
+    set --global fish_color_param 575F66
+    set --global fish_color_quote 86B300
+    set --global fish_color_error F51818
+    set --global fish_color_escape 4CBF99
+    set --global fish_color_normal 575F66
+    set --global fish_color_command 55B4D4
+    set --global fish_color_comment ABB0B6
+    set --global fish_color_operator FF9940
+    set --global fish_color_cancel --reverse
+    set --global fish_color_host_remote yellow
+    set --global fish_color_redirection A37ACC
+    set --global fish_color_autosuggestion 8A9199
+    set --global fish_color_history_current --bold
+    set --global fish_color_valid_path --underline
+    set --global fish_pager_color_completion normal
+
+    ## abbr
     abbr -a fa 'unshare -rn -- fastfetch -c all -l small'
     abbr -a gl 'git log --graph --decorate --oneline'
     abbr -a gpf 'git push --force-with-lease'
@@ -26,9 +56,16 @@ if status is-interactive
     abbr -a cc 'wl-copy --clear'
     abbr -a pdf 'zathura --fork'
 
+    ## misc
+    set --global fish_greeting ""
+
+    ## bind
+    set -g fish_key_bindings fish_default_key_bindings
+
     bind ctrl-x 'wl-copy --clear'
     bind \el "commandline --insert 'less -mnwic '"
 
+    ## func
     function mcd
         echo cd (string repeat -n \
         (math (string length -- $argv[1]) - 1) ../)
